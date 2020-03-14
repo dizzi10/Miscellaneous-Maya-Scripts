82220e892e03f969f6d6c0d2d82d16e052006765
dizzi10 <61439441+dizzi10@users.noreply.github.com>
2020-03-13 22:15:44 -0400

    Update InstanceAlongVertices.py

--- InstanceAlongVertices.py
+++ InstanceAlongVertices.py
@@ -1,35 +1,71 @@
 import maya.cmds as cmds
+import random
+from functools import partial
 
-#get all the components to instance over
-#set variable to allow user to pick which component type through UI
-def componentSelection(componentType = "Faces"):
-    if componentType = "Edges":
-        components = cmds.filterExpand(selectionMask = 32)
-    elif componentType = "Faces":
-        components = cmds.filterExpand(selectionMask = 34)
-    elif componentType = "Vertices"
-        components = cmds.filterExpand(selectionMask = 31)
-    return components
-#main function that perfoms the actual processes
-def instanceIteration():
+random.seed(1234)
     
+
+#main function that perfoms the actual processes
+def instanceIteration(RotationRangeField, ScaleRangeEndField, ScaleRangeStartField, PlacementAmountField):
+    
+    #Takes user input from the gui and stores them into variables
+    XRotationRange = cmds.floatFieldGrp(RotationRangeField, query = True, value1 - True)
+    YRotationRange = cmds.floatFieldGrp(RotationRangeField, query = True, value2 - True)
+    ZRotationRange = cmds.floatFieldGrp(RotationRangeField, query = True, value3 - True)
+    RotationRange = [XRotationRange, YRotationRange, ZRotationRange]
+    
+    XScaleRangeEnd = cmds.floatFieldGrp(ScaleRangeEndField, query = True, value1 - True)
+    YScaleRangeEnd = cmds.floatFieldGrp(ScaleRangeEndField, query = True, value2 - True)
+    ZScaleRangeEnd = cmds.floatFieldGrp(ScaleRangeEndField, query = True, value3 - True)
+    ScaleRangeEnd = [XScaleRangeEnd, YScaleRangeEnd, ZScaleRangeEnd]
+    
+    XScaleRangeStart = cmds.floatFieldGrp(ScaleRangeStartField, query = True, value1 - True)
+    YScaleRangeStart  = cmds.floatFieldGrp(ScaleRangeStartField, query = True, value2 - True)
+    ZScaleRangeStart  = cmds.floatFieldGrp(ScaleRangeStartField, query = True, value3 - True)
+    ScaleRangeStart  = [XScaleRangeStart , YScaleRangeStart , ZScaleRangeStart]
+    
+    PlacementAmountFactor = cmds.floatField(PlacementAmountField, query = True, value - True)
+    
+    #get all the components to instance over
+    #gets faces, edges and vertices
+    components = cmds.filterExpand(selectionMask = (31,34,32))
+            
     #get object that will be instanced
     objectlist = cmds.ls(selection = True, dagObjects = True , exactType = "mesh")
     object = objectlist[0]
     
-    #get components to be iterated over
-    components = componentSelection()
+    #Optional parameter that randomizes component placement
+    if PlacementAmount != 1:
+        PlacementAmount = (len(compenents)) * (PlacementAmountFactor) 
+        components = random.choices(components, k = PlacementAmount)
+    
+        
+ 
     
     #iterate over each vertex, creating rivet and instance
     instanceGroupName=cmds.group(empty=True, name=object + "_instance_grp#")
-    instancedict=dict()
     
     for component in components:
         cmds.select(deselect = True)
-        instancedict[component] = cmds.instance(object,name=object + "_instance#" , smartTransform = False)
-        cmds.parent(instancedict[component],instanceGroupName)
-        cmds.select(component, instancedict[component] , add = True)
-        cmds.pointOnPolyConstraint(component, instancedict[component])
+        currentInstance = cmds.instance(object, name = object + "_instance#")
+        cmds.parent(currentInstance, instanceGroupName)
+        cmds.select(component, currentInstance , add = True)
+        cmds.pointOnPolyConstraint(component, currentInstance)
+        # calls function to randoimize instance if requested
+        instanceRandomizer(currentInstance, RotationRange, ScaleRangeEnd, ScaleRangeStart, PlacementAmountFactor)
+# Optional settings to randomize instances
+def instanceRandomizer(currentInstance, RotationRange, ScaleRangeEnd, ScaleRangeStart, PlacementAmountFactor):
+    
+    xRot=random.uniform(0, RotationRange[0]) # this will be added to UI parameters
+    yRot=random.uniform(0, RotationRange[1]) # this will be added to UI parameters
+    zRot=random.uniform(0, RotationRange[2]) # this will be added to UI parameters
+    cmds.rotate(xRot,yRot,zRot, currentInstance)
+    xScale = random.uniform(ScaleRangeStart[0], ScaleRangeEnd[0]) # this will be added to UI parameters
+    yScale = random.uniform(ScaleRangeStart[1], ScaleRangeEnd[1]) # this will be added to UI parameters
+    zScale = random.uniform(ScaleRangeStart[2], ScaleRangeEnd[2]) # this will be added to UI parameters
+    cmds.scale(xScale, yScale, zScale, currentInstance)
+    cmds.xform(currentInstance,centerPivots=True)
+    return currentInstance
 
 #Create UI for script
 def UI():
@@ -38,12 +74,28 @@ def UI():
     
     if cmds.window( windowID, exists=True ):
         cmds.deleteUI( windowID )
-    cmds.window( windowID, title = "PointonPolyConstraintInstancer", sizeable=False, resizeToFitChildren=True )
-    cmds.rowColumnLayout(numberOfColumns = 2)
+    cmds.window( windowID, title = "PointonPolyConstraintInstancer", sizeable=False, resizeToFitChildren=True)
+    cmds.rowColumnLayout(numberOfColumns = 4)
     
-
-    
-    cmds.button(label="Apply",command = "changeNames(objectList)")
-    
-    cmds.showWindow()
+    cmds.separator( width=1, style='none' )
+    cmds.text(label = "Random Rotation Range:", align = "right")
+    cmds.separator( w=1, style='none' )
+    RotationRangeField = cmds.floatFieldGrp( numberOfFields=3, value1 = 0, value2 = 0, value3 = 0 )
    
+    cmds.separator( w=1, style='none' )
+    cmds.text(label = "Random Scale Range:", align = "right")
+    cmds.separator( width=1, style='none' )
+    ScaleRangeField = cmds.floatFieldGrp( numberOfFields=3, value1 = 0, value2 = 0, value3 = 0 )
+    
+    cmds.separator( w=1, style='none' )
+    cmds.text(label = "Random Component Placement Value:", align = "right")
+    cmds.separator( width=1, style='none' )
+    PlacementAmountField = cmds.floatField(value = 1, minValue = 0, maxValue = 1) 
+
+    cmds.separator( w=1, style='none' )
+    cmds.button(label="Apply", command=functools.partial(instanceIteration, RotationRangeField, 
+                                                         ScaleRangeField, PlacementAmountField )) 
+    cmds.button(label="Cancel")
+    cmds.showWindow()
+
+UI(instanceIteration)
